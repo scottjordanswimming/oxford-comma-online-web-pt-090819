9d6ae451e16d03f88bf753a22435c7038760dd64
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-18 19:50:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,13 @@
 def oxford_comma(array)
   
-  if array
+  if array.count == 1
+    
+    puts array.join
+    
+  else
   
   array[-1] = "and #{array[-1]}"
 
-return array.join(", ")
+puts array.join(", ")
 
 end
