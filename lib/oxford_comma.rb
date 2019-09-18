a5e258b195f7e9a0cef1eb5b405aeb5e8b5c2d7e
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-18 19:53:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  
+  if array.count == 1
+    
+    return array.join
+    
+  elsif array.count == 2 
+    
+    return array.join(" and ")
+    
+  else
+  
+  array[-1] = "and #{array[-1]}"
+
+return array.join(", ")
+
+end
 
 end
